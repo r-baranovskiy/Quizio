ec3010fb9dd8c514fc3c23fa3aa5b3783a48cc9a
Ruslan Baranovskiy <rus.baranovskiy@gmail.com>
2026-08-04 23:17:17 +0300

    git init

---
+++ Quizio/QuizioApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizioApp.swift
+//  Quizio
+//
+//  Created by Ruslan Baranovskiy on 04.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct QuizioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
